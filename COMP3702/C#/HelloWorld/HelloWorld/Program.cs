ef9fb5f8e316a1a527396bb1549728eb2f9ba48e
Nasser Mohamed Nasser Alsubhi <u113157@Student.squ.edu.om>
2018-09-19 14:26:44 +0400

    kk

---
+++ COMP3702/C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+
+        }
+    }
+}
