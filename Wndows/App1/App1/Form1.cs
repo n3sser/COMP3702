ccd6bb510d8e1de155b23c4cc3a8d796cdb3a783
Nasser Mohamed Nasser Alsubhi <u113157@Student.squ.edu.om>
2018-10-02 09:08:38 +0400

    jj

---
+++ Wndows/App1/App1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace App1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            String user = textBox1.Text;
+           
+            String password = textBox2.Text;
+            int pass = password.GetHashCode();
+
+            if (user.Equals("test") && pass =="123".GetHashCode())
+
+            {
+                label3.Text = "";
+                this.Hide();
+                Form2 f = new Form2(this, user);
+                f.Show();
+            }
+            else {
+
+                label3.Text = "Wrong username or password";
+
+                
+            }
+
+        }
+
+
+    }
+}
---
+++ Wndows/App1/App1/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace App1
+{
+    public partial class Form2 : Form
+    {
+        Form1 f1;
+        string username;
+        public Form2(Form1 f, string name)
+        {
+            f1 = f;
+            username = name;
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hello " + username;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            f1.Show();
+        }
+    }
+}
